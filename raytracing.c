8b65702b46bb328d9d6f765824519bd699a78cee
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2025-09-17 14:27:41 -0400

    rays generated completed

--- raytracing.c
+++ raytracing.c
@@ -6,12 +6,51 @@
 #define HEIGHT 600
 #define COLOR_WHITE 0xFFFFFFFF
 #define COLOR_BLACK 0x00000000
+#define RAYS_NUMBER 100
+#define COLOR_GRAY 0x80808080
 
 typedef struct Circle {
   double x;
   double y;
   double r;
 } Circle;
+
+// 57th minute we stopped. he is debugging  until 102
+
+typedef struct Ray {
+  double x_start, y_start;
+  double angle;
+} Ray;
+
+void FillRays(SDL_Surface *surface, Ray rays[RAYS_NUMBER], Uint32 color) {
+  for (int i = 0; i < RAYS_NUMBER; i++) {
+    Ray ray = rays[i];
+    int end_of_screen = 0;
+    int object_hit = 0;
+    double step = 1;
+
+    double x_draw = ray.x_start;
+    double y_draw = ray.y_start;
+    while (!end_of_screen && !object_hit) {
+      x_draw += cos(ray.angle) * step;
+      y_draw += sin(ray.angle) * step;
+      SDL_Rect pixel = (SDL_Rect){x_draw, y_draw, 1, 1};
+      SDL_FillRect(surface, &pixel, color);
+      if (x_draw < 0 || x_draw > WIDTH || y_draw < 0 || y_draw > HEIGHT) {
+        end_of_screen = 1;
+      }
+      if (object_hit) {
+        object_hit = 1;
+      }
+    }
+  }
+}
+void generate_rays(Circle circle, Ray rays[RAYS_NUMBER]) {
+  for (int i = 0; i < RAYS_NUMBER; i++) {
+    double angle = (double)(i)*2 * M_PI / RAYS_NUMBER;
+    rays[i] = (Ray){circle.x, circle.y, angle};
+  }
+}
 void FillCircle(SDL_Surface *surface, Circle circle, Uint32 color) {
 
   double radius_squared = pow(circle.r, 2);
@@ -39,8 +78,11 @@ int main() {
 
   SDL_Surface *surface = SDL_GetWindowSurface(window);
   Circle circle = (Circle){200, 200, 80};
+  Circle shadow = (Circle){650, 300, 140};
   SDL_Rect erase_rect = (SDL_Rect){0, 0, WIDTH, HEIGHT};
 
+  Ray rays[RAYS_NUMBER];
+  generate_rays(circle, rays);
   // Event loop
   SDL_Event event;
   int running = 1;
@@ -52,10 +94,14 @@ int main() {
       if (event.type == SDL_MOUSEMOTION && event.motion.state != 0) {
         circle.x = event.motion.x;
         circle.y = event.motion.y;
+        generate_rays(circle, rays);
       }
     }
     SDL_FillRect(surface, &erase_rect, COLOR_BLACK);
+    FillRays(surface, rays, COLOR_GRAY);
+
     FillCircle(surface, circle, COLOR_WHITE);
+    FillCircle(surface, shadow, COLOR_WHITE);
     SDL_UpdateWindowSurface(window);
     SDL_Delay(16);
   }
